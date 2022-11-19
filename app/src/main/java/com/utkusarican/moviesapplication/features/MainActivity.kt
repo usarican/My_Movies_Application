5c274bca433ebc83f38aff714960da88c360e0b8
usarican <iu.sarican@gmail.com>
2022-11-19 10:28:28 +0300

    Modularization

--- app/src/main/java/com/utkusarican/moviesapplication/features/MainActivity.kt
+++ app/src/main/java/com/utkusarican/moviesapplication/features/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.utkusarican.moviesapplication
+package com.utkusarican.moviesapplication.features
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.utkusarican.moviesapplication.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
